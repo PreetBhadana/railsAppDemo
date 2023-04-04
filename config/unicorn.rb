7376499b50ffdbcbcf10cdcfa888aa00ca689fed
Preet Bhadana <preetbhadana5@gmail.com>
2023-04-04 13:10:30 +0530

    help

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,4 +18,5 @@ stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set proccess id path
-pid "#{shared_dir}/pids/unicorn.pid"
+# pid "#{shared_dir}/pids/unicorn.pid"
+pid "#{app_dir}/tmp/pids/unicorn.pid"
